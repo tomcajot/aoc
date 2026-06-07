a4e53745b8709d2c21ce17771706a839207d9c9e
Tom Cajot <ctom@tudelft.nl>
2026-06-07 18:10:17 +0200

    renammed a file

--- d1p2/d1p2.sv
+++ d1p2/d1p2.sv
@@ -27,15 +27,6 @@ module d1p2 #(parameter INPUT_SIZE=4483) (input logic clk, rst_n, input logic [1
         if (state == counting) count <= count + 15'b1;
     end
 
-    always_ff @(posedge clk) begin
-        if (!rst_n) password <= 15'b0;
-        else begin
-            if (value == 20'b0) password <= password + 1'b1;
-            if (state == reducing) password <= password + 1'b1;
-            if (state == counting && value != 20'b0 && (value[19] != next_value[19])) password <= password + 1'b1;
-        end
-    end
-
     always_comb begin
         if (state == idle) next_value = 20'd50;
         if (state == counting) begin
@@ -46,8 +37,8 @@ module d1p2 #(parameter INPUT_SIZE=4483) (input logic clk, rst_n, input logic [1
             end
         end
         if (state == reducing) begin
-            if (next_value[19] == 1'b1) next_value = value + 20'd100;
-            else next_value = value - 20'd100;
+            if (value[19] == 1'b1) next_value = value + 20'sd100;
+            else next_value = value - 20'sd100;
         end
     end
 
@@ -55,12 +46,13 @@ module d1p2 #(parameter INPUT_SIZE=4483) (input logic clk, rst_n, input logic [1
         case (state)
             idle: next_state = counting;
             counting: begin
-                if (next_value > 20'sd99 || next_value < -20'sd99) next_state = reducing;
-                else if (count == INPUT_SIZE) next_state = done;
+                if (next_value > 20'sd99 || next_value[19] == 1'b1) next_state = reducing;
+                else if (count >= INPUT_SIZE) next_state = done;
                 else next_state = counting;
             end
             reducing: begin
-                if (next_value > 20'sd99  || next_value < -20'sd99) next_state = reducing;
+                if (next_value > 20'sd99  || next_value[19] == 1'b1) next_state = reducing;
+                else if (count >= INPUT_SIZE) next_state = done;
                 else next_state = counting;
             end
             done: begin
@@ -68,5 +60,17 @@ module d1p2 #(parameter INPUT_SIZE=4483) (input logic clk, rst_n, input logic [1
             end
         endcase
     end
+
+    always_ff @(posedge clk) begin
+        if (!rst_n) password <= 15'b0;
+        else begin
+
+            if (state == counting && (value == 20'b0)) password <= password + 1'b1;
+            if (state == counting && (value != 20'b0) && (value[19] != next_value[19])) password <= password + 1'b1;
+            if ((state == reducing) && (value > 20'sd99 || value < -20'sd99) && (next_value != 20'b0)) password <= password + 1'b1;
+
+        end
+    end
+
 
 endmodule
